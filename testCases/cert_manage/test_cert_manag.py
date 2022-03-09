babb9d567c035449c5626e114489d4376d382634
‘zhangxiaoxiao’ <jz_xiao@outlook.com>
2022-03-09 11:22:55 +0800

    增加新增证书接口，yaml文件中上传字符串换行

--- testCases/cert_manage/test_cert_manag.py
+++ testCases/cert_manage/test_cert_manag.py
@@ -17,7 +17,7 @@ class TestUserGroups:
     def test_get_cert_list(self, caseinfo):
         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
 
-    # @allure.story("新增证书")
-    # @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/save_cert_list.yaml'))
-    # def test_save_cert_list(self, caseinfo):
-    #     RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+    @allure.story("新增证书")
+    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/save_cert_list.yaml'))
+    def test_save_cert_list(self, caseinfo):
+        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
