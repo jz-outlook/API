cacec713a6ebea824fa36a55e3e47cb3ddcbb875
‘zhangxiaoxiao’ <jz_xiao@outlook.com>
2022-03-09 19:31:28 +0800

    api_login数据驱动

--- testCases/cert_manage/test_cert_manag.py
+++ testCases/cert_manage/test_cert_manag.py
@@ -1,33 +1,33 @@
-import allure
-import pytest
-
-from Hotloads.test import Test
-from common.parameterize_util import read_case_yaml
-from common.requests_util import RequestsUtil
-
-from Hotloads.debug_talk import DebugTalk
-
-
-@allure.epic("接口自动化测试")
-@allure.feature("证书管理模块")
-class TestUserGroups:
-
-    @allure.story("获取证书列表")
-    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/get_cert_list.yaml'))
-    def test_get_cert_list(self, caseinfo):
-        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
-
-    @allure.story("新增证书")
-    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/save_cert.yaml'))
-    def test_save_cert_list(self, caseinfo):
-        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
-
-    @allure.story("编辑证书")
-    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/edit_cert.yaml'))
-    def test_edit_cert(self, caseinfo):
-        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
-
-    @allure.story("删除证书")
-    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/delete_cert.yaml'))
-    def test_delete_cert(self, caseinfo):
-        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+# import allure
+# import pytest
+#
+# from Hotloads.test import Test
+# from common.parameterize_util import read_case_yaml
+# from common.requests_util import RequestsUtil
+#
+# from Hotloads.debug_talk import DebugTalk
+#
+#
+# @allure.epic("接口自动化测试")
+# @allure.feature("证书管理模块")
+# class TestUserGroups:
+#
+#     @allure.story("获取证书列表")
+#     @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/get_cert_list.yaml'))
+#     def test_get_cert_list(self, caseinfo):
+#         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+#
+#     @allure.story("新增证书")
+#     @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/save_cert.yaml'))
+#     def test_save_cert_list(self, caseinfo):
+#         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+#
+#     @allure.story("编辑证书")
+#     @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/edit_cert.yaml'))
+#     def test_edit_cert(self, caseinfo):
+#         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+#
+#     @allure.story("删除证书")
+#     @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/cert_manage/delete_cert.yaml'))
+#     def test_delete_cert(self, caseinfo):
+#         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
--- testCases/user_group_manage/test_group_manag.py
+++ testCases/user_group_manage/test_group_manag.py
@@ -1,38 +1,38 @@
-import allure
-import pytest
-
-from Hotloads.test import Test
-from common.parameterize_util import read_case_yaml
-from common.requests_util import RequestsUtil
-
-from Hotloads.debug_talk import DebugTalk
-
-
-@allure.epic("接口自动化测试")
-@allure.feature("用户组管理模块")
-class TestUserGroups:
-
-    @allure.story("获取用户组列表")
-    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/get_group_list.yaml'))
-    def test_get_group_list(self, caseinfo):
-        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
-
-    @allure.story("新增用户组")
-    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/save_group.yaml'))
-    def test_save_group(self, caseinfo):
-        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
-
-    @allure.story("修改用户组")
-    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/edit_group.yaml'))
-    def test_edit_group(self, caseinfo):
-        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
-
-    @allure.story("查询用户组")
-    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/get_group.yaml'))
-    def test_get_group(self, caseinfo):
-        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
-
-    @allure.story("删除用户组")
-    @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/delete_group.yaml'))
-    def test_delete_group(self, caseinfo):
-        RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+# import allure
+# import pytest
+#
+# from Hotloads.test import Test
+# from common.parameterize_util import read_case_yaml
+# from common.requests_util import RequestsUtil
+#
+# from Hotloads.debug_talk import DebugTalk
+#
+#
+# @allure.epic("接口自动化测试")
+# @allure.feature("用户组管理模块")
+# class TestUserGroups:
+#
+#     @allure.story("获取用户组列表")
+#     @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/get_group_list.yaml'))
+#     def test_get_group_list(self, caseinfo):
+#         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+#
+#     @allure.story("新增用户组")
+#     @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/save_group.yaml'))
+#     def test_save_group(self, caseinfo):
+#         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+#
+#     @allure.story("修改用户组")
+#     @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/edit_group.yaml'))
+#     def test_edit_group(self, caseinfo):
+#         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+#
+#     @allure.story("查询用户组")
+#     @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/get_group.yaml'))
+#     def test_get_group(self, caseinfo):
+#         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
+#
+#     @allure.story("删除用户组")
+#     @pytest.mark.parametrize('caseinfo', read_case_yaml('./testCases/user_group_manage/delete_group.yaml'))
+#     def test_delete_group(self, caseinfo):
+#         RequestsUtil('base_api_url', DebugTalk()).standard_yaml(caseinfo)
