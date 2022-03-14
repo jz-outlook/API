4185e95bd479582987595aef698680fac5c77832
‘zhangxiaoxiao’ <jz_xiao@outlook.com>
2022-03-14 17:24:22 +0800

    啥啥

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 import os
-import time
+
 import pytest
 
-from config.test1 import test_email
 
 if __name__ == '__main__':
-    pytest.main(['-vs'])
+    # pytest.main(['-vs'])
     # # # allyre 生成测试报告
-    # pytest.main(['-vs', '--alluredir', './temps'])
-    # os.system("allure generate ./temps -o ./reports --clean")
+    pytest.main(['-vs', '--alluredir', './temps'])
+    os.system("allure generate ./temps -o ./reports --clean")
     # test_email()
